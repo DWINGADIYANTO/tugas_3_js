cdc236396b6b3c03cac398964773ccaa426af03e
Dwi Ngadiyanto <dwingadiyanto879@gmail.com>
2023-02-09 21:47:51 +0700

    Tugas 3 Javascript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas3.js
@@ -0,0 +1,13 @@
+function ambil(){
+  var lempar = "ditangkap"
+  return"terambil";
+}
+var tampung = ambil();
+console.log(tampung);
+
+function mintadata(x,y,z){
+  console.log(x);
+  console.log(y);
+  console.log(z);
+}
+mintadata("hello","apakabar","sehat");
